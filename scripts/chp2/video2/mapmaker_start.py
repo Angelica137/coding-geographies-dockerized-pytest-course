a6b36a96e6976d024ca3766e8edb139a4d8b4b0c
Angelica <Angelica137@users.noreply.github.com>
2020-06-20 13:53:30 +0100

    pass map, remove __init__.py file

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,8 @@
 class Point():
-    pass
+    def __init__(self, name, latitude, longitude):
+        self._name = name
+        self._longitude = longitude
+        self._latitude = latitude
+
+    def get_lat_long(self):
+        return (self._latitude, self._longitude)
--- tests/chp2/video2/test_mapmaker_start.py
+++ tests/chp2/video2/test_mapmaker_start.py
@@ -1,4 +1,6 @@
+from scripts.chp2.video2.mapmaker_start import Point
 
 
 def test_make_one_point():
-    pass
+    p1 = Point("Dakar", 14.7167, 17.4677)
+    assert p1.get_lat_long() == (14.7167, 17.4677)
