15fe638d9c8a64d31fec779062a6306543523246
Angelica <Angelica137@users.noreply.github.com>
2020-06-20 15:11:38 +0100

    add correct test to vid3 remove form vid2

--- scripts/chp2/video3/mapmaker_exceptions_start.py
+++ scripts/chp2/video3/mapmaker_exceptions_start.py
@@ -1,10 +1,12 @@
 
 class Point():
     def __init__(self, name, latitude, longitude):
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
+        self._name = name
 
+        if not(-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+            raise ValueError("Invalid combination")
+        self._longitude = longitude
+        self._latitude = latitude
 
     def get_lat_long(self):
-        return (self.latitude, self.longitude)
+        return (self._latitude, self._longitude)
--- tests/chp2/video3/test_exceptions_start.py
+++ tests/chp2/video3/test_exceptions_start.py
@@ -1,4 +1,5 @@
 from scripts.chp2.video3.mapmaker_exceptions_start import Point
+import pytest
 
 
 def test_make_one_point():
@@ -6,7 +7,7 @@ def test_make_one_point():
     assert p1.get_lat_long() == (14.7167, 17.4677)
 
 
-def test_invalid_point_generation():  # TO DO
-    # with pytest.raises(Exception) as exp:
-    #     raise(Exception)
-    pass
+def test_invalid_pount_generation():
+    with pytest.raises(ValueError) as exp:
+        Point("Buenos Aires", 12.11386, -555.08269)
+    assert str(exp.value) == "Invalid combination"
