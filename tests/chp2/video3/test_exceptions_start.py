10c633776394bd54165298f94639cf816e814ffd
Angelica <Angelica137@users.noreply.github.com>
2020-06-20 15:13:11 +0100

    fixed typo

--- tests/chp2/video3/test_exceptions_start.py
+++ tests/chp2/video3/test_exceptions_start.py
@@ -7,7 +7,7 @@ def test_make_one_point():
     assert p1.get_lat_long() == (14.7167, 17.4677)
 
 
-def test_invalid_pount_generation():
+def test_invalid_point_generation():
     with pytest.raises(ValueError) as exp:
         Point("Buenos Aires", 12.11386, -555.08269)
     assert str(exp.value) == "Invalid combination"
