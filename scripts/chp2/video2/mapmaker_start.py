4243bf5287d749422015499cef59119b4b2c105d
Angelica <Angelica137@users.noreply.github.com>
2020-06-20 15:03:17 +0100

    add exception

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -2,7 +2,8 @@ class Point():
     def __init__(self, name, latitude, longitude):
         self._name = name
 
-        # if not(-90 <= latitude <= 90) or
+        if not(-90 <= latitude <= 90) or not (-180 <= longitude <= 180):
+            raise ValueError("Invalid combination")
         self._longitude = longitude
         self._latitude = latitude
 
